c200d4a29c74a7fad72f2ca1d805ed0b74a412ec
brittni.bresee@gmail.com <111385077+brittnibresee@users.noreply.github.com>
2023-03-29 11:53:06 -0500

    work_complete

---
+++ Leaflet_part-1/static/js/logic.js
@@ -0,0 +1,73 @@
+// Earthquake URL for all earthquakes in the last month
+const DATA_URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+
+d3.json(DATA_URL).then(function(data){
+    let earth_map = L.map("map", {
+      center: [38.89511, -100.03637],
+      zoom: 5
+    });
+  
+    console.log(data);
+
+    // Add layer to map
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+    }).addTo(earth_map);
+
+    // Map features
+    data.features.forEach(function(feature) {
+        var coords = feature.geometry.coordinates;
+        var mag = feature.properties.mag;
+        var depth = coords[2];
+        var color = depth > 90 ? "#800026" :
+                    depth > 70 ? "#BD0026" :
+                    depth > 50 ? "#E31A1C" :
+                    depth > 30 ? "#FC4E2A" :
+                    depth > 10 ? "#FD8D3C" :
+                                "#FEB24C";
+        var radius = mag * 5;
+        var marker = L.circleMarker([coords[1], coords[0]], {
+            radius: radius,
+            fillColor: color,
+            fillOpacity: 0.7,
+            color: "green",
+            weight: 0.5
+        });
+        marker.bindPopup("<h3>Magnitude: " + mag + "</h3><h3>Depth: " + depth + "</h3>").addTo(earth_map);
+    });
+
+    // Add legend to map
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+        depths = [-10, 10, 30, 50, 70, 90],
+        labels = [];
+
+        // Look through and generate label
+        for(var i =0; i < depths.length; i++) {
+            div.innerHTML +=
+            '<i style="background:' + getColor(depths[i] + 1) + '"></i> ' +
+            depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
+
+        }
+
+        return div;
+    };
+
+    legend.addTo(earth_map);
+
+});
+
+// Function to get color based on depth
+function getColor(d) {
+    return d > 90 ? "#800026" :
+           d > 70 ? "#BD0026" :
+           d > 50 ? "#E31A1C" :
+           d > 30 ? "#FC4E2A" :
+           d > 10 ? "#FD8D3C" :
+                    "#FEB24C";
+}
+
+
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # leaflet_geological_survey
  In this project I was tasked with developing a way to visualize USGS data that will allow them to better educate the public and other government organizations (and hopefully secure more funding) on issues facing our planet.
+
+## Overview
+This project uses Leaflet.js to display geological survey data on a map. The data is loaded via an API and visualized using D3.js.
+
+## Installation
+1. Clone the repository: `git clone https://github.com/your-username/your-repo.git`
+2. Navigate to the project directory: `cd your-repo`
+3. Install dependencies: `npm install`
+
+## Usage
+1. Start the development server: `npm start`
+2. Open a web browser and navigate to `http://localhost:3000`
+
+## Technologies Used
+- Leaflet.js
+- D3.js
+- HTML/CSS
+- JavaScript
+
+## Credits
+- Geological survey data provided by XYZ API
+- Leaflet.js documentation: https://leafletjs.com
+- D3.js documentation: https://d3js.org
+
+## License
+This project is licensed under the MIT License.
